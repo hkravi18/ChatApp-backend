26b84d490df4b2a7d3ea7b38d242056e9221ca4f
Harshit Kumar Ravi <96059039+hkravi18@users.noreply.github.com>
2023-06-28 18:38:01 +0530

    Update frontend URL in index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
     cors: {
-        origin: "https://chatapp-y06o.onrender.com/",
+        origin: "https://chat-app-zus4.onrender.com/",
         credentials: true, 
     }
 })
